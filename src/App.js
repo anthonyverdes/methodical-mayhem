e7c93c9597cc81d63400c7ed9ea504faa3036c51
Anthony <verdes.anthony@gmail.com>
2020-01-08 11:37:55 -0600

    Clean up default react code, add base of homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="banner">
+        <h1>Methodical Mayhem</h1>
+        <h3>Bleeding Hollow</h3>
+      </div>
+      <div className="promo-area">
+        <p>A friendly and super laid back guild from Bleeding Hollow! Feel free to chat in-game, ask questions, and enjoy the game with others! We are down to do pretty much anything to make a friend and lets have fun!</p>
+      </div>
+      <div className="promo-area">
+        <h3>Discord Info</h3>
+        <p>If you're looking for our discord info, you've come to the right place.</p>
+        <a href="https://discord.gg/p3b8hfG">https://discord.gg/p3b8hfG</a>
+      </div>
+      <div className="promo-area">
+        <h3>Recruitment</h3>
+        <p>We're always on the lookout for fun, friendly people. But we are currently interested in tanks and healers for our core raiding group.</p>
+        <p>We raid on Wednesdays at 8:35pm EST with clean up scheduled for the Monday before reset.</p>
+      </div>
+      <footer>
+        <p>&copy; 2020 | Methodical Mayhem | All Rights Reserved</p>
+      </footer>
     </div>
   );
 }
